a5973da275611df766cbb262ed5b6c865b650049
Aly Ahmed <aly.ahmed@outlook.com>
2026-04-06 11:44:37 +0200

    feat: scale canvas to fill window with FIT mode

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ new Phaser.Game({
   width: 800,
   height: 400,
   backgroundColor: '#1a1a2e',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 800,
+    height: 400
+  },
   physics: {
     default: 'arcade',
     arcade: { gravity: { y: 600 }, debug: false }
